78c9d448f0a9ea9c324544e64af1949f19dab29e
vvd170501 <36827317+vvd170501@users.noreply.github.com>
2023-04-04 01:55:37 +0300

    Fix checkout test

--- ngit/cli/checkout.py
+++ ngit/cli/checkout.py
@@ -1,24 +1,33 @@
 import click
 
-from ..backend import NodeId
 from ..context import get_context
 from ..core.nodes import NodeName, resolve_named_node
-from ..core.refs import get_branch_events, parse_ref, ref_to_str, set_head
+from ..core.refs import RefId, get_branch_events, parse_ref, set_head
 from .common import require_repo
 
 
 @click.command()
 @click.argument('target')
 @require_repo
-def checkout(**kwargs):
-    ref, branch = do_checkout(**kwargs)
-    if branch:
-        click.echo(f'Switched to branch \'{branch}\'')
-    else:
-        click.echo(f'HEAD is now at {ref_to_str(ref)} (detached)')
+def checkout(target: str):
+    if not target:
+        raise click.ClickException('Ref cannot be empty')
+    if try_checkout_branch(target):
+        click.echo(f'Switched to branch \'{target}\'')
+        return
+
+    try:
+        ref = parse_ref(target)
+    except Exception:
+        raise click.ClickException(f'Unknown target: "{target}" is not a branch name or commit id')
+    if try_checkout_ref(ref):
+        click.echo(f'HEAD is now at {target} (detached)')
+        return
+
+    raise click.ClickException(f'Unknown target: "{target}" is not a branch name or commit id')
 
 
-def do_checkout(target: str) -> tuple[NodeId, str]:
+def try_checkout_branch(target: str) -> bool:
     target_branch = None
     for branch in get_branch_events():
         if branch.name == target:
@@ -26,18 +35,24 @@ def do_checkout(target: str) -> tuple[NodeId, str]:
             # no break, need to get the latest event. TODO use reverse order?
     if target_branch is not None:
         set_head(target_branch.ref, target_branch.name)
-        return target_branch.ref, target_branch.name
+        return True
+    return False
 
-    try:
-        ref = parse_ref(target)
-    except Exception:
-        raise click.ClickException(f'Unknown target: "{target}" is not a branch name or commit id')
 
+def try_checkout_ref(ref: RefId) -> bool:
     commit_tree = resolve_named_node(NodeName.COMMIT_TREE)
     for node in get_context().server.get_nodes(commit_tree):
         # TODO use something better. Direct get?
         if node.id == ref:
             set_head(ref, '')
-            return ref, ''
+            return True
+    return False
 
-    raise click.ClickException(f'Unknown target: "{target}" is not a branch name or commit id')
+
+# Wrappers for tests
+def checkout_branch(branch: str) -> None:
+    assert try_checkout_branch(branch), f'Branch {branch} doesn\'t exist'
+
+
+def checkout_ref(ref: RefId) -> None:
+    assert try_checkout_ref(ref), f'Ref {ref} doesn\'t exist'
--- tests/test_commit_history.py
+++ tests/test_commit_history.py
@@ -1,7 +1,7 @@
 import pytest
 
 from ngit.cli.branch import create_branch
-from ngit.cli.checkout import do_checkout
+from ngit.cli.checkout import checkout_branch, checkout_ref
 from ngit.cli.commit import create_commit
 from ngit.core.refs import RefId, iterate_history
 
@@ -12,7 +12,7 @@ class TestCommitHistory(NGitTest):
     @pytest.fixture(autouse=True)
     def setup(self, init_repo, mock_context: Context) -> None:  # init_repo is requested for correct init order
         create_branch('main')
-        do_checkout('main')
+        checkout_branch('main')
 
     def expect_history(self, expected_history: list[tuple[RefId, str]], commit: RefId | None = None):
         if commit is None:
@@ -43,7 +43,7 @@ class TestCommitHistory(NGitTest):
         main2 = create_commit('main2')
         assert self.active_branch == 'main'
 
-        do_checkout('test')
+        checkout_branch('test')
         assert self.active_branch == 'test'
         self.expect_history([])
         test1 = create_commit('test1')
@@ -53,19 +53,18 @@ class TestCommitHistory(NGitTest):
         assert test2 != main2
 
         # Check that we can switch back
-        do_checkout('main')
+        checkout_branch('main')
         self.expect_history([
             (main2, 'main2'),
             (main1, 'main1'),
         ])
         # Switch to test again, just in case
-        do_checkout('test')
+        checkout_branch('test')
         self.expect_history([
             (test2, 'test2'),
             (test1, 'test1'),
         ])
 
-    @pytest.mark.skip(reason='Need to separate ref parsing and checkout')
     def test_commit_and_checkout_detached(self):
         assert self.active_branch == 'main'
         main1 = create_commit('main1')
@@ -73,9 +72,11 @@ class TestCommitHistory(NGitTest):
         assert self.active_branch == 'main'
 
         # Detach and create some commits
-        do_checkout(main1)
+        checkout_ref(main1)
         assert not self.active_branch
-        self.expect_history([main1, 'main1'])
+        self.expect_history([
+            (main1, 'main1'),
+        ])
         fork1 = create_commit('fork1')
         fork2 = create_commit('fork2')
         assert fork2 != main2
@@ -86,7 +87,7 @@ class TestCommitHistory(NGitTest):
         ])
 
         # check that main wasn't moved and that we still can commit to it
-        do_checkout('main')
+        checkout_branch('main')
         assert self.head == main2
         main3 = create_commit('main3')
         self.expect_history([
